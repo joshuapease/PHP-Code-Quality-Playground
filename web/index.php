61aa7a112563cfb8ca02f812e31fb6af3ef1f332
Joshua Pease <joshua.pease@viget.com>
2023-08-07 11:33:26 -0700

    Set up composer

--- web/index.php
+++ web/index.php
@@ -1,3 +1,15 @@
 <?php
 
+require '../vendor/autoload.php';
+
 echo "Hello world";
+
+$mapped = collect([
+    'a',
+    'b',
+    'c'
+])
+    ->map(fn(string $x) => $x . '-mapped')
+    ->all();
+
+print_r($mapped);
